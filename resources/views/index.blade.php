d3fd13a4f2feedc006e4e28da6759b90b49a2390
Tiago Alves <tiagooliveiraaso2@gmail.com>
2023-11-26 21:50:04 -0300

    layout main created

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,121 +1,75 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layout.main')
 
-<head>
-    <meta charset="utf-8">
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
-
-    <title>FlexStart Bootstrap Template - Index</title>
-    <meta content="" name="description">
-    <meta content="" name="keywords">
-
-    <!-- Favicons -->
-    <link href="/img/favicon.png" rel="icon">
-    <link href="/img/apple-touch-icon.png" rel="apple-touch-icon">
-
-    <!-- Google Fonts -->
-    <link
-        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
-        rel="stylesheet">
-
-    <link rel="stylesheet" href="{{ asset('js/swiper/swiper-bundle.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('js/glightbox/css/glightbox.min.css') }}">
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-</head>
-
-<body>
-
-    <!-- ======= Header ======= -->
-    <header id="header" class="header fixed-top">
-        @include('include.menu')
-    </header><!-- End Header -->
-
+@section('before-content')
     <!-- ======= Hero Section ======= -->
     <section id="hero" class="hero d-flex align-items-center">
         @include('include.section.hero')
     </section><!-- End Hero -->
+@endsection
+@section('content')
+<section id="about" class="about">
+    @include('include.section.about')
+</section>
+<!-- End About Section -->
 
-    <main id="main">
-        <!-- ======= About Section ======= -->
-        <section id="about" class="about">
-            @include('include.section.about')
-        </section>
-        <!-- End About Section -->
+<!-- ======= Values Section ======= -->
+<section id="values" class="values">
+    @include('include.section.values')
+</section><!-- End Values Section -->
 
-        <!-- ======= Values Section ======= -->
-        <section id="values" class="values">
-            @include('include.section.values')
-        </section><!-- End Values Section -->
+<!-- ======= Counts Section ======= -->
+<section id="counts" class="counts">
+    @include('include.section.counts')
+</section><!-- End Counts Section -->
 
-        <!-- ======= Counts Section ======= -->
-        <section id="counts" class="counts">
-            @include('include.section.counts')
-        </section><!-- End Counts Section -->
+<!-- ======= Features Section ======= -->
+<section id="features" class="features">
+    @include('include.section.features')
+</section><!-- End Features Section -->
 
-        <!-- ======= Features Section ======= -->
-        <section id="features" class="features">
-            @include('include.section.features')
-        </section><!-- End Features Section -->
+<!-- ======= Services Section ======= -->
+<section id="services" class="services">
+    @include('include.section.services')
+</section><!-- End Services Section -->
 
-        <!-- ======= Services Section ======= -->
-        <section id="services" class="services">
-            @include('include.section.services')
-        </section><!-- End Services Section -->
+<!-- ======= Pricing Section ======= -->
+<section id="pricing" class="pricing">
+    @include('include.section.pricing')
+</section><!-- End Pricing Section -->
 
-        <!-- ======= Pricing Section ======= -->
-        <section id="pricing" class="pricing">
-            @include('include.section.pricing')
-        </section><!-- End Pricing Section -->
+<!-- ======= F.A.Q Section ======= -->
+<section id="faq" class="faq">
+    @include('include.section.ask')
+</section><!-- End F.A.Q Section -->
 
-        <!-- ======= F.A.Q Section ======= -->
-        <section id="faq" class="faq">
-            @include('include.section.ask')
-        </section><!-- End F.A.Q Section -->
+<!-- ======= Portfolio Section ======= -->
+<section id="portfolio" class="portfolio">
+    @include('include.section.projects')
+</section><!-- End Portfolio Section -->
 
-        <!-- ======= Portfolio Section ======= -->
-        <section id="portfolio" class="portfolio">
-            @include('include.section.projects')
-        </section><!-- End Portfolio Section -->
+<!-- ======= Testimonials Section ======= -->
+<section id="testimonials" class="testimonials">
+    @include('include.section.testimonials')
+</section><!-- End Testimonials Section -->
 
-        <!-- ======= Testimonials Section ======= -->
-        <section id="testimonials" class="testimonials">
-            @include('include.section.testimonials')
-        </section><!-- End Testimonials Section -->
+<!-- ======= Team Section ======= -->
+<section id="team" class="team">
+    @include('include.section.team')
+</section><!-- End Team Section -->
 
-        <!-- ======= Team Section ======= -->
-        <section id="team" class="team">
-            @include('include.section.team')
-        </section><!-- End Team Section -->
+<!-- ======= Clients Section ======= -->
+<section id="clients" class="clients">
+    @include('include.section.clients-logo')
+</section><!-- End Clients Section -->
 
-        <!-- ======= Clients Section ======= -->
-        <section id="clients" class="clients">
-            @include('include.section.clients-logo')
-        </section><!-- End Clients Section -->
+<!-- ======= Recent Blog Posts Section ======= -->
+<section id="recent-blog-posts" class="recent-blog-posts">
+    @include('include.section.blog-post')
+</section><!-- End Recent Blog Posts Section -->
 
-        <!-- ======= Recent Blog Posts Section ======= -->
-        <section id="recent-blog-posts" class="recent-blog-posts">
-            @include('include.section.blog-post')
-        </section><!-- End Recent Blog Posts Section -->
+<!-- ======= Contact Section ======= -->
+<section id="contact" class="contact">
+    @include('include.section.contact')
+</section><!-- End Contact Section -->
+@endsection
 
-        <!-- ======= Contact Section ======= -->
-        <section id="contact" class="contact">
-            @include('include.section.contact')
-        </section><!-- End Contact Section -->
-
-    </main><!-- End #main -->
-
-    <!-- ======= Footer ======= -->
-    <footer id="footer" class="footer">
-        @include('include.footer')
-    </footer><!-- End Footer -->
-
-    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
-            class="bi bi-arrow-up-short"></i></a>
-
-    <!-- Vendor JS Files -->
-    <script src="{{ asset('js/swiper/swiper-bundle.min.js') }}"></script>
-    <script src="{{ asset('js/glightbox/glightbox.min.js') }}"></script>
-    <script src="{{ asset('js/plugins.js') }}"></script>
-</body>
-
-</html>
