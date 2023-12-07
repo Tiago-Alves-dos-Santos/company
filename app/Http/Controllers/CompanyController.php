379ad57db507fc87c7695fd4707659c7343f12af
Tiago Alves <tiagooliveiraaso2@gmail.com>
2023-12-07 12:09:41 -0300

    company page created

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class CompanyController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('admin.company');
+    }
 }
---
+++ resources/views/admin/company.blade.php
@@ -0,0 +1,13 @@
+@extends('layout.admin', ['tab_title' => 'Empresa'])
+
+@section('title', 'Empresa')
+
+@section('content')
+    <div>
+        <div class="flex justify-evenly flex-wrap">
+            <x-card.header title="Soluções Software">
+
+            </x-card.header>
+        </div>
+    </div>
+@endsection
