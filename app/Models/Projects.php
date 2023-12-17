7d99012e81a702c0e22d399cbd20c9a114d35526
Tiago Alves <tiagooliveiraaso2@gmail.com>
2023-12-17 18:32:50 -0300

    relações e softdeletes em projeto e categoria

--- app/Models/Projects.php
+++ app/Models/Projects.php
@@ -2,10 +2,24 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Projects extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+    /** =========================MUTATORS=========================== */
+
+    /**========================RELATIONSHIP===========================*/
+    public function projectsCategory(): BelongsTo
+    {
+        return $this->belongsTo(ProjectCategory::class);
+    }
+    /** ========================= METHODS STATIC =========================== */
+
+    /** ========================= METHODS =========================== */
+
+
 }
