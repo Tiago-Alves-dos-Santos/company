e62cb4732a59cbe8bde089cf4ecec875464adadf
Tiago Alves <tiagooliveiraaso2@gmail.com>
2023-12-19 17:25:48 -0300

    listando empresas de cliente

--- app/Livewire/Admin/CustomerCompany/Table.php
+++ app/Livewire/Admin/CustomerCompany/Table.php
@@ -2,12 +2,15 @@
 
 namespace App\Livewire\Admin\CustomerCompany;
 
+use App\Models\CustomerCompany;
 use Livewire\Component;
 
 class Table extends Component
 {
     public function render()
     {
-        return view('livewire.admin.customer-company.table');
+        return view('livewire.admin.customer-company.table',[
+            'customers_company' => CustomerCompany::cursor()
+        ]);
     }
 }
--- resources/views/livewire/admin/customer-company/table.blade.php
+++ resources/views/livewire/admin/customer-company/table.blade.php
@@ -12,8 +12,49 @@
                             <th scope="col" class="px-6 py-4">Ações</th>
                         </tr>
                     </thead>
-                    <tbody>
-                        <tr class="border-b dark:border-neutral-500">
+                    <tbody >
+                        @forelse ($customers_company as $value)
+                        <tr class="border-b dark:border-neutral-500" data-te-lightbox-init>
+                            <td class="px-6 py-4 whitespace-nowrap">{{ $value->name }}</td>
+                            <td class="px-6 py-4 whitespace-nowrap">{{ $value->client_name }}</td>
+                            <td class="px-6 py-4 whitespace-nowrap" >
+                                <img src="{{ asset("img/customer_company/".$value->logo) }}"  data-te-img="{{ asset("img/customer_company/".$value->logo) }}"
+                                data-te-caption="{{ $value->logo }}"
+                                class="text-blue-600 w-[100px]">
+                            </td>
+                            <td class="flex px-6 py-4 whitespace-nowrap">
+                                <x-custom.button context="warning" icon="ri-edit-line text-lg mr-2" class="mr-2"
+                                    link="">
+                                    Editar
+                                </x-custom.button>
+                                <x-custom.button type="button" context="danger" :load_livewire="true"
+                                    icon="ri-delete-bin-line text-lg mr-2" wire:loading.attr="disabled"
+                                    x-on:confirm="{
+                                            title: 'Deseja continuar com a ação?',
+                                            description: 'Isso apagará também todas as imagens do projeto',
+                                            icon: 'question',
+                                            accept: {
+                                                label: 'Confirmar',
+                                                method: 'delete',
+                                                params: ''
+                                            },
+                                            reject: {
+                                                label: 'Cancelar',
+                                            }
+                                        }">
+                                    Excluir
+                                    <x-slot:load>
+                                        <div wire:loading wire:target='x'>
+                                            <x-custom.load></x-custom.load>
+                                        </div>
+                                    </x-slot>
+                                </x-custom.button>
+                            </td>
+                        </tr>
+                        @empty
+
+                        @endforelse
+                        {{-- <tr class="border-b dark:border-neutral-500">
                             <td class="px-6 py-4 whitespace-nowrap">Mark</td>
                             <td class="px-6 py-4 whitespace-nowrap">Otto</td>
                             <td class="px-6 py-4 whitespace-nowrap">@mdo</td>
@@ -45,7 +86,7 @@
                                     </x-slot>
                                 </x-custom.button>
                             </td>
-                        </tr>
+                        </tr> --}}
 
                     </tbody>
                 </table>
