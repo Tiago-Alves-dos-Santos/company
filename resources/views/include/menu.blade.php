9c9398caa82b4c5c70ba52287d9f7c0da7130363
Tiago Alves <tiagooliveiraaso2@gmail.com>
2024-01-03 22:55:47 -0300

    ajuste titulo modal login

--- resources/views/include/menu.blade.php
+++ resources/views/include/menu.blade.php
@@ -79,7 +79,7 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h1 class="modal-title fs-5" id="loginClientLabel">Entrar</h1>
+                <h1 class="modal-title fs-5" id="loginClientLabel">@if (!AuthClient::check()) Entrar @else Encerrar @endif</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
